27d7aefbd612259f596129553cd216b2bc5228d6
AbdElrahman Sakr <aa_sakr@hotmail.com>
2013-03-27 13:43:09 +0200

    switch copy

---
+++ Switch - Copy/Switch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            string carModel;
+            string carManufacturer="sj";
+
+            System.Console.Write("Please Enter Your Vehicle Model: ");
+
+            carModel = System.Console.ReadLine();
+
+            switch (carModel)
+            {
+                case "cerato":
+                case "rio":
+                case "soul":
+                    carManufacturer = "Kia";
+                    break;
+                case "focus":
+                    carManufacturer = "Ford";
+                    break;
+                case "corolla":
+                    carManufacturer = "Toyota";
+                    break;
+                default:
+
+                    break;
+            }
+
+            System.Console.Write("Manufacturer is " + carManufacturer + "\n");
+
+            //  carManufacturer = "0";
+            Main();
+        }
+
+
+
+
+
+
+
+    }
+}
+
